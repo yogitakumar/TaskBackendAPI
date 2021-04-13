8f9befb5f6115acab37c7ef87af3fd17f9c2720f
yogitakumar <helloyogita@gmail.com>
2021-04-13 21:40:57 +0100

    Error solved with APIGatewayProxyRequestEvent and APIGatewayProxyResponseEvent

--- src/main/java/com/impact/Handler.java
+++ src/main/java/com/impact/Handler.java
@@ -1,17 +1,14 @@
 package com.impact;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.impact.model.Task;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
@@ -23,14 +20,13 @@ public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIG
 		Task t1 = new Task("001", "buy milk", true);
 		Task t2 = new Task("002", "mend the table", false);
 		Task t3 = new Task("003", "shop grocery", true);
-		//Task t4 = null;
-		//System.out.println(t4.getDescription());
+
 		List<Task> tasks = new ArrayList<Task>();
 		tasks.add(t1);
 		tasks.add(t2);
 		tasks.add(t3);
-APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-response.setStatusCode(200);
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setStatusCode(200);
 		return response;
 	}
 }
